db2ca9cbcf0be21c429d2e37bdc44bc404e64d75
seagullcanfly666 <32128629+seagullcanfly666@users.noreply.github.com>
2017-10-21 10:58:05 +1300

    new shot

--- scripts.js
+++ scripts.js
@@ -3,3 +3,13 @@ $( document ).ready(function(){
 
 
 });
+
+function showSection(){
+$(".example-section").fadeIn(2000);
+}
+
+setTimeout(
+  function(){
+    $(".example-section").css("border-bottom","1px solid black");
+},3000);
+}
