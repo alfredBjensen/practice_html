8fdd3dde59754faef1f8b544d9b1ce5e8dd615a5
alfredBjensen <alfredBjensen@gmail.com>
2017-10-20 16:54:01 -0500

    updated
    update  javascript

--- scripts.js
+++ scripts.js
@@ -1,5 +1,22 @@
 $( document ).ready(function(){
 
+    //all code will go here
+    showSection();
+
 
 
 });
+
+
+
+function showSection(){
+
+  $(".example-section").fadeIn(2000);
+
+
+  setTimeout(
+    function(){
+      $(".example-section").css('border-bottom', "1px solid black");
+    }, 3000);
+
+}
